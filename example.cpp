d1a09b474f6d461d7369c4387df05515934fc8f9
ztsvvstz1337 <unbekannter771@gmail.com>
2026-02-14 00:03:45 +0100

    fixed leveldb, webserver and added miniagent shizo agent framework in shizotech/system/miniagent

--- example.cpp
+++ example.cpp
@@ -2,6 +2,9 @@
 #ifdef _WIN32
 #include <Windows.h>
 
+//TODO: let claude opus create a CABI bridge so I can finally release the source and others can make modules
+//Let it handle the abstraction and force it to use stable CABI interfaces only which dont expose my internal data structures
+
 BOOL APIENTRY DllMain(HMODULE hModule,
     DWORD  ul_reason_for_call,
     LPVOID lpReserved
